e075f3b2ac1db0ae52e2de01d764aba5bbe5f830
AndreyRazin <110714747+AndreyRazin007@users.noreply.github.com>
2023-07-31 16:40:54 +0500

    Created Game Engine, added project engine folder, added test functionality in engine, added script files for building the project for Windows and Linux.

---
+++ engine/src/test.c
@@ -0,0 +1,8 @@
+#include "test.h"
+
+#include <stdio.h>
+
+void print_int(i32 number)
+{
+    printf("The number is: %i\n", number);
+}
---
+++ testbed/src/main.c
@@ -0,0 +1,12 @@
+#include <test.h>
+
+#include <stdlib.h>
+
+int main(void)
+{
+    print_int(228);
+
+    system("pause");
+
+    return 0;
+}
